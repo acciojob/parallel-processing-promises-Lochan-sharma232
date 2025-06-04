e179bdc09c969bc4d7b14f5edbb41143cac944ba
Lochan-sharma232 <lochansharma537@gmail.com>
2025-06-04 14:07:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //your JS code here. If required.
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
-const imgerr=document.getElementById("error");
-const 
-const imageUrls = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
-];
+// const output = document.getElementById("output");
+// const btn = document.getElementById("download-images-button");
+// const imgerr=document.getElementById("error");
+// const 
+// const imageUrls = [
+//   { url: "https://picsum.photos/id/237/200/300" },
+//   { url: "https://picsum.photos/id/238/200/300" },
+//   { url: "https://picsum.photos/id/239/200/300" },
+// ];
 
 const imageUrls = [
   { url: "https://picsum.photos/id/237/200/300" },
